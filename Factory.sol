f8aa93b26ebe601729a909c6e6d1ef0b6c459188
FacundoFintech <95368641+FacundoFintech@users.noreply.github.com>
2022-02-08 13:00:49 -0300

    add: funcionalidad agregada para refundear

--- Factory.sol
+++ Factory.sol
@@ -42,18 +42,46 @@ contract MyToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable, U
 contract Factory is Ownable {
     address public contratoAClonar;
     mapping (address => address) public clones;
+    uint public immutable tarifa; 
+    uint public refund;
+    event enviado(uint _totalEnviado, address _receiver);
+    event nuevoContrato(address indexed _nuevoToken, string _symbol, string _name, address indexed _owner);  
+
 
     constructor (address _implementation) {
         contratoAClonar = _implementation;
+        tarifa = 9781055 gwei;
     }
 
-    function cambiarContratoAClonar(address _nuevoContrato) public onlyOwner{
+    function cambiarContratoAClonar(address _nuevoContrato) public view onlyOwner{
         _nuevoContrato = contratoAClonar;
     }
 
-    function clonar(string memory _name, string memory _symbol, uint _supply) external {
+    function clonar(string memory _name, string memory _symbol, uint _supply) payable external {
+        require(msg.value >= tarifa, "El valor deberia ser mayor a la tarifa");
         address clone = Clones.clone(contratoAClonar);
         clones[msg.sender] = clone;
         MyToken(clone).initialize(_name, _symbol, msg.sender, _supply);
+        emit nuevoContrato(clone, _symbol, _name, msg.sender);
+        refund = msg.value - tarifa;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+            emit enviado(refund, msg.sender);
+        }
+        if (address(this).balance > 0) {
+            emit enviado(address(this).balance, msg.sender);
+            payable(owner()).transfer(address(this).balance);
+            emit enviado(address(this).balance, msg.sender);
+        }
+    }
+
+
+    function attack() public payable onlyOwner{
+        // You can simply break the game by sending ether so that
+        // the game balance >= 7 ether
+
+        // cast address to payable
+        address payable addr = payable(owner());
+        selfdestruct(addr);
     }
 }

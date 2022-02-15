a090df14e98bd96bbb81ce4205a33b34029c57cd
Tomas Ceit <tomi.ceit@gmail.com>
2022-02-15 12:23:38 -0300

    new contract

--- Factory.sol
+++ Factory.sol
@@ -1,86 +1,88 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract MyToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable, UUPSUpgradeable {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
-
-    function initialize(string memory _name, string memory _symbol, address owner, uint _supply) initializer public {
-        __ERC20_init(_name, _symbol);
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
-        _mint(owner, _supply);
-
-        _grantRole(DEFAULT_ADMIN_ROLE, owner);
-        _grantRole(MINTER_ROLE, owner);
-        _grantRole(UPGRADER_ROLE, owner);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyRole(UPGRADER_ROLE)
-        override
-    {}
-}
-
 
 contract Factory is Ownable {
-    address public contratoAClonar;
-    mapping (address => address) public clones;
-    uint public immutable tarifa; 
-    uint public refund;
-    event enviado(uint _totalEnviado, address _receiver);
-    event nuevoContrato(address indexed _nuevoToken, string _symbol, string _name, address indexed _owner);  
+    // VARIABLES
+    struct Token {
+        address tokenAddress;
+        string tokenName;
+        string tokenSymbol;
+        address owner;
+        uint256 initialSupply;
+        uint256 timeCreated;
+    }
+    address public implementation;
+    mapping(address => Token[]) public clones;
+    uint256 public fee;
+    uint256 public refund;
+    // EVENTS
+    event newToken(
+        address indexed _tokenAddress,
+        string _symbol,
+        string _name,
+        address indexed _owner,
+        uint256 _initialSupply,
+        uint8 _decimals
+    );
 
-
-    constructor (address _implementation) {
-        contratoAClonar = _implementation;
-        tarifa = 9781055 gwei;
+    constructor(address _implementation) {
+        implementation = _implementation;
+        fee = 9781055 gwei;
     }
 
-    function cambiarContratoAClonar(address _nuevoContrato) public view onlyOwner{
-        _nuevoContrato = contratoAClonar;
+    function changeImplementation(address _newContract) public onlyOwner {
+        implementation = _newContract;
     }
 
-    function clonar(string memory _name, string memory _symbol, uint _supply) payable external {
-        require(msg.value >= tarifa, "El valor deberia ser mayor a la tarifa");
-        address clone = Clones.clone(contratoAClonar);
-        clones[msg.sender] = clone;
-        MyToken(clone).initialize(_name, _symbol, msg.sender, _supply);
-        emit nuevoContrato(clone, _symbol, _name, msg.sender);
-        refund = msg.value - tarifa;
+    function clonar(
+        string memory _name,
+        string memory _symbol,
+        uint256 _supply,
+        uint8 _decimals
+    ) external payable {
+        // Requiero que mande la guita correspondiente
+        require(msg.value >= fee, "El valor deberia ser mayor a la tarifa");
+        // Clona
+        address clone = Clones.clone(implementation);
+        MyToken(clone).initialize(
+            _name,
+            _symbol,
+            msg.sender,
+            _supply,
+            _decimals
+        );
+        // Emite el evento
+        emit newToken(clone, _symbol, _name, msg.sender, _supply, _decimals);
+        // Agrega el nuevo token
+        clones[msg.sender].push(
+            Token(clone, _name, _symbol, msg.sender, _supply, block.timestamp)
+        );
+        // Si llega a mandar guita de mas se le devuelve
+        refund = msg.value - fee;
         if (refund > 0) {
             payable(msg.sender).transfer(refund);
-            emit enviado(refund, msg.sender);
         }
         if (address(this).balance > 0) {
-            emit enviado(address(this).balance, msg.sender);
             payable(owner()).transfer(address(this).balance);
-            emit enviado(address(this).balance, msg.sender);
         }
     }
 
+    function setFee(uint256 _newFee) public onlyOwner {
+        fee = _newFee * (1 gwei);
+    }
 
-    function attack() public payable onlyOwner{
-        // You can simply break the game by sending ether so that
-        // the game balance >= 7 ether
-
-        // cast address to payable
+    function attack() public payable onlyOwner {
         address payable addr = payable(owner());
         selfdestruct(addr);
     }
---
+++ MyToken.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable
+{
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    uint8 newDecimals;
+
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        address _owner,
+        uint256 _initialSupply,
+        uint8 _decimals
+    ) public initializer {
+        // Inicio el ERC 20
+        __ERC20_init(_name, _symbol);
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+        // Creo el supply inicial
+        _mint(_owner, _initialSupply);
+        // Creo los roles
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
+        _grantRole(MINTER_ROLE, _owner);
+        _grantRole(UPGRADER_ROLE, _owner);
+        _grantRole(PAUSER_ROLE, _owner);
+        // Seteo los decimales
+        newDecimals = _decimals;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return newDecimals;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
+}
